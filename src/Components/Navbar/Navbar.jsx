5c171a952722f40b9a459854728d0a99ac535a94
Attaboy <174675467+Attaboy20@users.noreply.github.com>
2024-08-11 15:34:56 -0500

    Buttons working and page layout updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,15 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className = 'nav'> 
+            <div nav className = "nav-logo">Asif Siddiqui Real Estate Agent </div>
+            <ul className="nav-menu">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>About</li>
+            <li className = 'nav-contact'> Contact</li>
+            </ul>
+        
         </div>
     )
 }
